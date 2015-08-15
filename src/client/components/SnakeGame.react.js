e375dc064c5eac8dc317f0f9b0e170d38a0c8428
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-08-15 16:45:35 -0700

    styles fixed

--- src/client/components/SnakeGame.react.js
+++ src/client/components/SnakeGame.react.js
@@ -1,14 +1,44 @@
 var Board = require('./Board.react');
 var React = require('react');
+var SnakeGameActions = require('../actions/SnakeGameActions');
 
 var SnakeGame = React.createClass({
 
 	render: function () {
+		$(function(){
+		    $('html').keypress(function(e){
+		        this._onKeyDown(e);
+		    });
+		});
 		return (
 			<div>
-				<Board />
+				<Board 
+				/>
 			</div>
 		);
+	},
+
+	_onKeyDown: function (event) {
+		var direction;
+		switch (event) {
+			case 38:
+				direction = "up";
+			case 39:
+				direction = "right";
+				break;
+			case 40:
+				direction = "down";
+				break;
+			case 37:
+				direction = "left";
+				break;
+			default:
+				direction = null;
+		}
+		if (direction != null) {
+			SnakeGameActions.changeDirection(direction);
+			console.log(direction);
+		}
 	}
 
 });
