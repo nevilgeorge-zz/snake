deedf8a5398e8bbafff3b4a6fa1f5c07f8e2967e
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-11-02 18:05:04 -0600

    changing callback functions to arrow functions

--- src/server/server.js
+++ src/server/server.js
@@ -1,74 +1,74 @@
 // server.js
-'use strict';
+'use strict'
 
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io  = require('socket.io')(http);
-const randomColor = require('randomcolor');
-const path = require('path');
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io  = require('socket.io')(http)
+const randomColor = require('randomcolor')
+const path = require('path')
 
-const Player = require('./models/Player');
+const Player = require('./models/Player')
 
-const clientSockets = {}; // socket -> Player
-const scoreTable = {}; // playerId -> player.score
-const players = [];
-const playerCount = 1;
+const clientSockets = {} // socket -> Player
+const scoreTable = {} // playerId -> player.score
+const players = []
+const playerCount = 1
 
-const SIDE_MAX = 30;
+const SIDE_MAX = 30
 
-app.set('port', 3000);
-app.set('view engine', 'html');
+app.set('port', 3000)
+app.set('view engine', 'html')
 
-app.use(express.static(path.join(__dirname + './../client')));
+app.use(express.static(path.join(__dirname + './../client')))
 
-app.get('/', function(req, res) {
-  res.sendfile(path.join(__dirname + './../client/index.html'));
-});
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname + './../client/index.html'))
+})
 
-app.get('/play', function(req, res) {
-  res.sendfile(path.join(__dirname + './../client/play.html'));
-});
+app.get('/play', (req, res) => {
+  res.sendfile(path.join(__dirname + './../client/play.html'))
+})
 
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
   // When new user joins, add player to room.
-  console.log('New player #' + playerCount + ' has joined the game.');
+  console.log('New player #' + playerCount + ' has joined the game.')
   let newPlayer = new Player({
     'playerId': playerCount,
     'score': 0,
     'color': randomColor()
-  });
-  scoreTable[playerCount] = 0;
-  clientSockets[socket] = newPlayer;
-  players.push(newPlayer);
-  playerCount += 1;
-  io.sockets.emit('update:players', players);
+  })
+  scoreTable[playerCount] = 0
+  clientSockets[socket] = newPlayer
+  players.push(newPlayer)
+  playerCount += 1
+  io.sockets.emit('update:players', players)
 
   // when user gets a fruit, update player data in all clients
-  socket.on('update', function(player) {
-    scoreTable[player.playerId] = player.score;
-    socket.emit('newScore', player);
-  });
+  socket.on('update', (player) => {
+    scoreTable[player.playerId] = player.score
+    socket.emit('newScore', player)
+  })
 
   // create food when required
-  socket.on('nofood', function(xMax, yMax) {
+  socket.on('nofood', (xMax, yMax) => {
     let coords = {
       x: Math.floor(Math.random() * xMax),
       y: Math.floor(Math.random() * yMax)
-    };
-    io.sockets.emit('update:food', coords);
-  });
+    }
+    io.sockets.emit('update:food', coords)
+  })
 
   // when user disconnects, remove that user from the score table
-  socket.on('disconnect', function() {
-    let player = clientSockets[socket];
-    delete scoreTable[player.playerId];
+  socket.on('disconnect', () => {
+    let player = clientSockets[socket]
+    delete scoreTable[player.playerId]
 
     console.log('Player #' + player.playerId + ' has left the game.')
-    io.sockets.emit('update:players', players);
-  });
-});
+    io.sockets.emit('update:players', players)
+  })
+})
 
-http.listen(app.get('port'), function() {
-  console.log('Listening on port ' + app.get('port') + '.');
-});
+http.listen(app.get('port'), () => {
+  console.log('Listening on port ' + app.get('port') + '.')
+})
